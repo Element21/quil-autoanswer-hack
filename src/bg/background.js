fbe39d7baf9418b9915950af710adb21937bc734
Aiden Marchiori <36175783+Element21@users.noreply.github.com>
2021-05-27 14:26:26 -0400

    Implement webEvent API

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,31 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+
+/**
+ * @filedescription Initializes the extension's background page.
+ */
+
 var settings = new Store("settings", {
   "sample_setting": "This is how you use Store.js to remember values"
 });
-// TODO: Wait for pageload event and send message to inject.js
-// Use chrome.webNavigation API
+
+var nav = new NavigationCollector();
+
+var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+  'onCommitted', 'onCompleted', 'onDOMContentLoaded',
+  'onErrorOccurred', 'onReferenceFragmentUpdated', 'onTabReplaced',
+  'onHistoryStateUpdated'];
+
+eventList.forEach(function (event) {
+  chrome.webNavigation[event].addListener(function (data) {
+    if (typeof data)
+      console.log(chrome.i18n.getMessage('inHandler'), event, data);
+    else
+      console.error(chrome.i18n.getMessage('inHandlerError'), event);
+  });
+});
+
+// Reset the navigation state on startup. We only want to collect data within a
+// session.
+chrome.runtime.onStartup.addListener(function () {
+  nav.resetDataStorage();
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,1 +1,4 @@
+/**
+ * @filedescription This is the code that gets injected into the site
+ */
 // TODO: When we recive the message from background.js, do the api login and start grabbing topics to solve
