a2cd3e890227bf4ae6d2c9260c7eac5083ee59bf
Aiden Marchiori <36175783+Element21@users.noreply.github.com>
2021-05-28 14:27:17 -0400

    Readd service worker to grab cookies and use i8n to communinicate cookie to content script

--- background-worker.js
+++ background-worker.js
@@ -1,5 +1,11 @@
 try {
-    importScripts("src/bg/background.js");
+    importScripts("src/inject/inject.js");
 } catch (e) {
     console.error(e);
 }
+
+chrome.webRequest.onCompleted.addListener(function (responseHeaders) {
+    console.log(responseHeaders)
+});
+
+// chrome.runtime.sendMessage({ quillSessionId: sessionId })
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,34 +1,22 @@
-/**
- * @filedescription This is the code that gets injected into the site
- */
+console.log("I've Been Injected!")
 
-function getCookie(domain, name, callback) {
-    chrome.cookies.get({ "url": domain, "name": name }, function (cookie) {
-        if (callback) {
-            callback(cookie.value);
-        }
-    });
-}
+// When we recive message from service worker with cookie, do juicy things
+// // create dictionary to hold the api request headers
+// let headers = {
+//     "User-Agent": "Mozilla / 5.0(X11; Linux x86_64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 91.0.4472.77 Safari / 537.36",
+//     "Accept-Language": "en-US,en;q=0.9",
+//     "Cookie": sessionId
+// };
 
-// Get current session id (changes on every api response)
-let sessionId = chrome.cookies.get({ url: 'https://quill.org', name: '_quill_session' })
-
-// create dictionary to hold the api request headers
-let headers = {
-    "User-Agent": "Mozilla / 5.0(X11; Linux x86_64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 91.0.4472.77 Safari / 537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Cookie": sessionId
-};
-
-// Get classes
-fetch('https://www.quill.org/students_classrooms_json', {
-    method: 'GET',
-    headers: JSON.stringify(headers)
-})
-    // Sync our clients cookies with the cookies from the api response
-    .then(JSON.stringify(response) => {
-        response.cookie.forEach((cookie) => {
-            chrome.cookie.set()
-        })
-            .then(data => console.log(data));
-        console.log(response)
+// // Get classes
+// let response = fetch('https://www.quill.org/students_classrooms_json', {
+//     method: 'GET',
+//     headers: JSON.stringify(headers)
+// })
+// // Sync our clients cookies with the cookies from the api response
+// if (response.ok) { // if HTTP-status is 200-299
+//     let classes = response.json();
+//     console.log(classes)
+// } else {
+//     alert("HTTP-Error: " + response.status);
+// }
