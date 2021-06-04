2c9314b9fcf729196718d4fe1721a956b18fe34f
Aiden Marchiori <aiden@alanmarian.com>
2021-06-03 21:14:03 -0400

    V1 (Playwright Browser Automation) Very basic features, basic school selection and custom code to navigate my schools sso (TODO: General login)

---
+++ index.js
@@ -0,0 +1,42 @@
+const {username, password} = require('./config.json')
+const { chromium } = require('playwright')
+const axios = require('axios');
+
+(async () => {
+    const browser = await chromium.launch({ headless: false })
+    const page = await browser.newPage()
+    // Login
+    await page.goto('https://www.quill.org/session/new')
+    await page.click('.auth-section > button:nth-child(2)')
+    await page.click('.Autosuggest--textInput')
+    await page.keyboard.type('lewisburg', { delay: 50 })
+    await page.waitForTimeout(1000)
+    await page.keyboard.press('Enter')
+    await page.click('a.flexbox:nth-child(1)')
+    await page.click('#userNameInput')
+    await page.keyboard.type(username)
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(password)
+    await page.keyboard.press('Enter')
+    // Subscribe to http responses to get question data
+    page.on('response', response => {
+        if (response.url().includes('responses')) {
+            axios.get(response.url())
+                .then(function (response) {
+                    page.click('#main-content > div > section > div.student-container.student-container-diagnostic > div > div > div.content > div.student.text-editor.card.is-fullwidth > div > div > div')
+                    for (let answerBlob in response.data) {
+                        // Check if answer is correct
+                        if (response.data[answerBlob].optimal) {
+                            console.log(response.data[answerBlob].text)
+                            page.keyboard.type(' ' + response.data[answerBlob].text, { delay: 50 })
+                            break
+                        }
+                    }
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+    });
+})();
