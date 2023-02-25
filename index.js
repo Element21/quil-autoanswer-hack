e5e48c73c17de9411188bb4878af6e787b41a18e
Aiden Marchiori <aiden@alanmarian.com>
2023-02-25 13:19:20 -0500

    Update login logic to work with all login methods

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-'use strict';
-
-const { username, password, schoolName } = require('./config.json')
-const { chromium } = require('playwright')
+const { chromium } = require('playwright');
 const axios = require('axios');
 
+let answerCount = 1;
+
 (async () => {
-    const browser = await chromium.launch({ headless: false })
-    const page = await browser.newPage()
-    // Login
-    await page.goto('https://www.quill.org/session/new')
-    await page.setDefaultNavigationTimeout(0);
-    await page.click('.auth-section > button:nth-child(2)')
-    await page.click('#autosuggest-input-')
-    await page.keyboard.type(schoolName, { delay: 50 })
-    await page.waitForTimeout(1000)
-    await page.keyboard.press('Enter')
-    // Custom code for my school's sso solution
-    await page.click('a.flexbox:nth-child(1)')
-    await page.click('#userNameInput')
-    await page.keyboard.type(username)
-    await page.keyboard.press('Tab')
-    await page.keyboard.type(password)
-    await page.keyboard.press('Enter')
-    // Subscribe to http responses to get question data
-    page.on('response', response => {
-        if (response.url().includes('responses')) {
-            axios.get(response.url())
-                .then(function (response) {
-                    page.click('#main-content > div > section > div.student-container.student-container-diagnostic > div > div > div.content > div.student.text-editor.card.is-fullwidth > div > div > div')
-                    for (let answerBlob in response.data) {
-                        // Check if answer is correct
-                        if (response.data[answerBlob].optimal) {
-                            console.log(response.data[answerBlob].text)
-                            // Fix weird behavior whene first character of text is not typed
-                            page.keyboard.type(' ' + response.data[answerBlob].text, { delay: 50 })
-                            break
-                        }
-                    }
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                });
-        }
-    });
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+  const usernameDropdownLocator = '#nav-user-dropdown > span';
+  page.setDefaultTimeout(0);
+  page.setDefaultNavigationTimeout(0);
+
+  // Login
+  await page.goto('https://www.quill.org/session/new');
+  console.log('Please signin to continue');
+  await page.waitForSelector(usernameDropdownLocator);
+  const name = await page.locator(usernameDropdownLocator).textContent();
+  console.log(`Login succesful! Welcome, ${name.trim()}!\n`);
+
+  // Subscribe to http responses to get question data
+  page.on('response', (response) => {
+    if (response.url().includes('responses')) {
+      axios.get(response.url())
+        .then((r) => {
+          // eslint-disable-next-line no-restricted-syntax
+          for (const answerBlob in r.data) {
+            // Check if answer is correct
+            if (r.data[answerBlob].optimal) {
+              console.log(`Answer ${answerCount}: ${r.data[answerBlob].text}`);
+              answerCount += 1;
+            }
+          }
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    }
+  });
 })();
